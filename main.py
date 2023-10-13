502f114197ab63c854af93187f265d29d070108e
Viktor <v.stepanishchev@bk.ru>
2023-10-13 19:56:41 +0300

    EditMain

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print(lox)
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+
+from handlers import router
+
+token = open("token.txt").readline()
+
+async def main() -> None:
+    bot = Bot(token=token, parse_mode="Markdown")
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+if __name__ == '__main__':
+    asyncio.run(main)
+
