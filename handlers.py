e49a68e93fe312b0e17cd5d9944bd055bd28e994
Viktor <v.stepanishchev@bk.ru>
2023-10-14 03:32:10 +0300

    InlineButtons

--- handlers.py
+++ handlers.py
@@ -1,3 +1,20 @@
+from aiogram.filters import Command
+from aiogram.types import Message, CallbackQuery
 from aiogram import F, Router
 
+import kb
+
 router = Router()
+@router.message(Command("start"))
+async def start(message: Message):
+    await message.answer(f"Привет, {message.from_user.username}, меня зовут Coursbot! Я смогу подсказать тебе текущий курс валюты. Выбери интересующую тебя валюту ниже!", reply_markup=kb.all_valuete_kb.as_markup())
+@router.callback_query(F.data == 'backmenu')
+async def a(clbck: CallbackQuery):
+    await clbck.message.answer(f"Привет, меня зовут Coursbot! Я смогу подсказать тебе текущий курс валюты. Выбери интересующую тебя валюту ниже!", reply_markup=kb.all_valuete_kb.as_markup())
+
+for n in kb.all_valuete_kb:
+    @router.callback_query(F.data == f'{n}')               # Попытка сделать обработку InlineButton через цикл, лучше делать вручную
+    async def a(clbck: CallbackQuery):
+        for i in kb.currency:
+            if F.data == i:
+                await clbck.message.edit_text(f'{i}', reply_markup=kb.all_valuete_kb.as_markup())
--- main.py
+++ main.py
@@ -8,11 +8,15 @@ token = open("token.txt").readline()
 
 async def main() -> None:
     bot = Bot(token=token, parse_mode="Markdown")
-    dp = Dispatcher(storage=MemoryStorage())
+    dp = Dispatcher(storage=MemoryStorage()) # ну здесь тоже все понятно блять
     dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    await bot.delete_webhook(drop_pending_updates=True) # Игнорирование сообшений до запуска бота вот
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types()) # Запуск бота
 
 if __name__ == '__main__':
-    asyncio.run(main)
+    asyncio.run(main()) #Запускаем main что не понятно то
 
+"""
+- создать callback клаву под сообщением которое выводится при команде /start  
+- в этих кнопках должны быть все спарсенные курсы валют 
+"""
